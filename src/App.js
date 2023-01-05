2fd1100ba4f462d6aa440508c83c78605ebd8c13
ericjen01 <eric.jen01@gmail.com>
2023-01-04 16:00:59 -0800

    component state, event handlers revisited

--- src/App.js
+++ src/App.js
@@ -1,49 +1,62 @@
+import { useState } from "react";
+
+const Hello = ({ name, age }) => {
+	//const name = props.name;
+	//const age = props.age;
+	//const { name, age } = props; destructing instead of assigning the props to constants
+	const bornYear = () => new Date().getFullYear() - age;
+
+	return (
+		<div>
+			<p>
+				Hello {name}, you are {age} years old
+			</p>
+			<p>You were born in {bornYear()}</p>
+		</div>
+	);
+};
+
+const Display = (props) => {
+	return <div>{props.clickCount}</div>;
+};
+
+const Button = ({ onClick, text }) => {
+	return <button onClick={onClick}>{text}</button>;
+};
+
 const App = () => {
-	const t = [1, -1, 3];
-	const t2 = t.concat("concat value");
-	const t3 = [1, 2, 3, 4, 5];
-	const [a, b, c, ...rest] = t3; //destructuring assignment
-	const Ruberto = {
-		name: "Reberto Fellas",
-		age: 35,
-		education: "hight school",
-		greet: function () {
-			console.log(`hello im ${this.name}, im ${this.age} years old`);
-		},
-		greet2: () => {
-			console.log("yaya");
-		},
-	};
+	const name = "peter";
+	const age = 10;
 
-	t.push(5);
-	Ruberto.greet();
-	Ruberto.greet2();
+	const [counter, setCounter] = useState(0);
+	const [clickCount, setClickCount] = useState(0);
+	const increaseByOne = () => setClickCount(clickCount + 1);
+	const setToZero = () => setClickCount(0);
+	const handleClick = () => console.log("clicked");
 
-	console.log("t.length: ", t.length); // 4 is printed
-	console.log("t[1]: ", t[1]); // -1 is printed
-	console.log("t2: ", t2); // array of t2 printed
-	console.log("a, b, c: ", a, b, c); // prints destructured values 1, 2 ,3
-	console.log("rest: ", rest); // prints the array of the rest of destructured value in the array
+	setTimeout(() => setCounter(counter + 1), 1000);
+	//console.log("component is rendered by the setTimeout ", counter);
 
-	t.forEach((value) => {
-		console.log("map value: ", value); // numbers 1, -1, 3, 5 are printed, each to own line
-	});
-
-	class Person {
-		constructor(name, age) {
-			this.name = name;
-			this.age = age;
-		}
-		greet() {
-			console.log("hello, my name is " + this.name);
-		}
-	}
-	const adam = new Person("Adam Ondra", 29);
-	adam.greet();
-	const janja = new Person("Janja Garnbret", 23);
-	janja.greet();
-
-	return;
+	return (
+		<div>
+			<h1>Greetings</h1>
+			<Hello name="Maya" age={26 + 10} />
+			<Hello name={name} age={age} />
+			<h1>Counter: {counter}</h1>
+			<button onClick={handleClick}>click</button>
+			<button onClick={() => setCounter(0)}>set to zero</button>
+			<div>
+				<h1>Click Count: {clickCount}</h1>
+				<button onClick={increaseByOne}>plus</button>
+				<button onClick={setToZero}>zero</button>
+			</div>
+			<h1>
+				<Display clickCount={clickCount} />
+				<Button onClick={setToZero} text="zero" />
+				<Button onClick={increaseByOne} text="increase +1" />
+			</h1>
+		</div>
+	);
 };
 
 export default App;
