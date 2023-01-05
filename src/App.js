26ca3f52584c6b05f7f542ea63181e599a0b9bfc
ericjen01 <eric.jen01@gmail.com>
2023-01-05 14:51:54 -0800

    completed reading through 1-d course notes

--- src/App.js
+++ src/App.js
@@ -1,60 +1,85 @@
 import { useState } from "react";
 
-const Hello = ({ name, age }) => {
-	//const name = props.name;
-	//const age = props.age;
-	//const { name, age } = props; destructing instead of assigning the props to constants
-	const bornYear = () => new Date().getFullYear() - age;
-
-	return (
-		<div>
-			<p>
-				Hello {name}, you are {age} years old
-			</p>
-			<p>You were born in {bornYear()}</p>
-		</div>
-	);
+const History = (props) => {
+	if (props.allClicks.length === 0) {
+		return <div>the app is used by pressing the buttons</div>;
+	}
+	return <div>button press history: {props.allClicks.join(", ")}</div>;
 };
 
-const Display = (props) => {
-	return <div>{props.clickCount}</div>;
-};
-
-const Button = ({ onClick, text }) => {
-	return <button onClick={onClick}>{text}</button>;
-};
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
 
 const App = () => {
-	const name = "peter";
-	const age = 10;
+	const [left, setLeft] = useState(0);
+	const [right, setRight] = useState(0);
+	const [clicks, setClicks] = useState({ left: 0, right: 0 });
+	const [clicks2, setClicks2] = useState({ left: 0, right: 0 });
+	const [allClicks, setAll] = useState([]);
+	const [clicks4, setClicks4] = useState({ left: 0, right: 0 });
 
-	const [counter, setCounter] = useState(0);
-	const [clickCount, setClickCount] = useState(0);
-	const increaseByOne = () => setClickCount(clickCount + 1);
-	const setToZero = () => setClickCount(0);
-	const handleClick = () => console.log("clicked");
+	const doLeftClick = () => {
+		const newClicks = {
+			left: clicks.left + 1,
+			right: clicks.right,
+		};
+		setClicks(newClicks);
+	};
+	const doRightClick = () => {
+		const newClicks = {
+			left: clicks.left,
+			right: clicks.right + 1,
+		};
+		setClicks(newClicks);
+	};
 
-	setTimeout(() => setCounter(counter + 1), 1000);
-	//console.log("component is rendered by the setTimeout ", counter);
+	const doLeftClick2 = () => setClicks2({ ...clicks2, left: clicks2.left + 1 });
+	const doRightClick2 = () => setClicks2({ ...clicks2, right: clicks2.right + 1 });
+
+	const doLeftClick3 = () => {
+		setAll(allClicks.concat("L"));
+		setLeft(left + 1);
+	};
+	const doRightClick3 = () => {
+		setAll(allClicks.concat("R"));
+		setRight(right + 1);
+	};
+
+	const doLeftClick4 = () => setClicks4({ ...clicks4, left: clicks4.left + 1 });
+	const doRightClick4 = () => setClicks4({ ...clicks4, right: clicks4.right + 1 });
 
 	return (
 		<div>
-			<h1>Greetings</h1>
-			<Hello name="Maya" age={26 + 10} />
-			<Hello name={name} age={age} />
-			<h1>Counter: {counter}</h1>
-			<button onClick={handleClick}>click</button>
-			<button onClick={() => setCounter(0)}>set to zero</button>
 			<div>
-				<h1>Click Count: {clickCount}</h1>
-				<button onClick={increaseByOne}>plus</button>
-				<button onClick={setToZero}>zero</button>
+				{left}
+				<button onClick={() => setLeft(left + 1)}>left</button>
+				<button onClick={() => setRight(right + 1)}>right</button>
+				{right}
 			</div>
-			<h1>
-				<Display clickCount={clickCount} />
-				<Button onClick={setToZero} text="zero" />
-				<Button onClick={increaseByOne} text="increase +1" />
-			</h1>
+			<div>
+				{clicks.left}
+				<button onClick={doLeftClick}>left</button>
+				<button onClick={doRightClick}>right</button>
+				{clicks.right}
+			</div>
+			<div>
+				{clicks2.left}
+				<button onClick={doLeftClick2}>left</button>
+				<button onClick={doRightClick2}>right</button>
+				{clicks2.right}
+			</div>
+			<div>
+				<History allClicks={allClicks} />
+				{left}
+				<button onClick={doLeftClick3}>left</button>
+				<button onClick={doRightClick3}>right</button>
+				{right}
+			</div>
+			<div>
+				{clicks4.left}
+				<Button handleClick={doLeftClick4} text="left" />
+				<Button handleClick={doRightClick4} text="right" />
+				{clicks4.right}
+			</div>
 		</div>
 	);
 };
