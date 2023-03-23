6da8b0c4898abb0a244aae618c9f954f3ebb5ca7
ericjen01 <eric.jen01@gmail.com>
2023-03-22 22:07:09 -0700

    5-c9 ~ 5-c13: finding the elements, test coverage...

--- frontend/src/components/NoteForm.js
+++ frontend/src/components/NoteForm.js
@@ -22,7 +22,7 @@ const NoteForm = ({ createNote }) => {
 			<h2>Create a new note</h2>
 
 			<form onSubmit={addNote}>
-				<input value={newNote} onChange={handleChange} />
+				<input value={newNote} onChange={handleChange} placeholder="write note content here" id="note-input" />
 				<button type="submit">save</button>
 			</form>
 		</div>
