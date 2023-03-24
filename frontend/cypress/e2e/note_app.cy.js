50d4d7e74ee6ca7a0a76a3fc76476d4825287781
ericjen01 <eric.jen01@gmail.com>
2023-03-24 00:33:54 -0700

    5-d3: testing new note form

--- frontend/cypress/e2e/note_app.cy.js
+++ frontend/cypress/e2e/note_app.cy.js
@@ -12,21 +12,6 @@ describe("Note app", function () {
 	});
 });
 
-describe("Note app", () => {
-	it("front page can be opened", () => {
-		cy.visit("http://localhost:3000");
-		cy.contains("Notes");
-		cy.contains("Note app, Department of Computer Science, University of Helsinki 2022");
-	});
-});
-
-describe("Note app", function () {
-	it("login form can be opened", function () {
-		cy.visit("http://localhost:3000");
-		cy.contains("log in").click();
-	});
-});
-
 describe("Note app", function () {
 	beforeEach(function () {
 		cy.visit("http://localhost:3000");
@@ -44,5 +29,21 @@ describe("Note app", function () {
 		cy.get("#password").type("salainen");
 		cy.get("#login-button").click();
 		cy.contains("Superuser logged in");
+	});
+
+	describe("when logged in", function () {
+		beforeEach(function () {
+			cy.contains("log in").click();
+			cy.get("#username").type("root");
+			cy.get("#password").type("salainen");
+			cy.get("#login-button").click();
+			cy.contains("Superuser logged in");
+		});
+		it("a new note can be created", function () {
+			cy.contains("new note").click();
+			cy.get("#note-input").type("a note created by cypress");
+			cy.contains("save").click();
+			cy.contains("a note created by cypress");
+		});
 	});
 });
