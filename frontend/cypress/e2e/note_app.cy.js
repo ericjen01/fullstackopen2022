59297d902a1ed8e67ddc1091949f909791adf968
ericjen01 <eric.jen01@gmail.com>
2023-03-23 23:43:43 -0700

    5-d2: writing a form

--- frontend/cypress/e2e/note_app.cy.js
+++ frontend/cypress/e2e/note_app.cy.js
@@ -19,3 +19,30 @@ describe("Note app", () => {
 		cy.contains("Note app, Department of Computer Science, University of Helsinki 2022");
 	});
 });
+
+describe("Note app", function () {
+	it("login form can be opened", function () {
+		cy.visit("http://localhost:3000");
+		cy.contains("log in").click();
+	});
+});
+
+describe("Note app", function () {
+	beforeEach(function () {
+		cy.visit("http://localhost:3000");
+	});
+	it("front page can be opened", function () {
+		cy.contains("Notes");
+		cy.contains("Note app, Department of Computer Science, University of Helsinki 2022");
+	});
+	it("login form can be opened", function () {
+		cy.contains("log in").click();
+	});
+	it("user can login", function () {
+		cy.contains("log in").click();
+		cy.get("#username").type("root");
+		cy.get("#password").type("salainen");
+		cy.get("#login-button").click();
+		cy.contains("Superuser logged in");
+	});
+});
