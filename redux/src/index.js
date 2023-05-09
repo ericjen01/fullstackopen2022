9d09a1ad2c21efe131039fa8e1ad3255666c001d
ericjen01 <eric.jen01@gmail.com>
2023-05-09 14:36:57 -0700

    6-d1: managing data on the servier with react query library

--- redux/src/index.js
+++ redux/src/index.js
@@ -1,51 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./store";
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import App from "./App";
 
-/*noteService.getAll().then((notes) => {
-	store.dispatch(setNotes(notes));
-	console.log("you don't need this anymore");
-});*/
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<Provider store={store}>
+	<QueryClientProvider client={queryClient}>
 		<App />
-	</Provider>
+	</QueryClientProvider>
 );
-
-/* 6-a2:
-const counterReducer = (state = 0, action) => {
-	switch (action.type) {
-		case "INCREMENT":
-			return state + 1;
-		case "DECREMENT":
-			return state - 1;
-		case "ZERO":
-			return 0;
-		default:
-			return state;
-	}
-};
-
-const store = createStore(counterReducer);
-
-const App = () => {
-	return (
-		<div>
-			<div>{store.getState()}</div>
-			<button onClick={(e) => store.dispatch({ type: "INCREMENT" })}>plus</button>
-			<button onClick={(e) => store.dispatch({ type: "DECREMENT" })}>minus</button>
-			<button onClick={(e) => store.dispatch({ type: "ZERO" })}>zero</button>
-		</div>
-	);
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const renderApp = () => {
-	root.render(<App />);
-};
-renderApp();
-store.subscribe(renderApp);
- */
