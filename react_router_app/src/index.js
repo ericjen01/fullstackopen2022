dd1ff8ef766cb8b2727cddcf9b6ff28dcd13a9ce
ericjen01 <eric.jen01@gmail.com>
2023-06-09 00:41:48 -0700

    7-c4: bootstrap & ui material

--- react_router_app/src/index.js
+++ react_router_app/src/index.js
@@ -1,6 +1,13 @@
 /* eslint-disable no-unused-vars */
-//import React from "react";
-//import ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate, useParams, useNavigate, useMatch } from "react-router-dom";
 
 //ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<Router>
+		<App />
+	</Router>
+);
