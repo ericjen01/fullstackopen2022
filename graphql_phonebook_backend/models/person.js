3a07a45486e4fde8ff79c7f75f0b602cca36421c
ericjen01 <ericjen01>
2023-07-08 16:45:26 -0700

    8-c2: validation, connected backend folder to mongoDB

---
+++ graphql_phonebook_backend/models/person.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  street: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  city: {
+    type: String,
+    required: true,
+    minlength: 2,
+  },
+  phone: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+});
+
+module.exports = mongoose.model("Person", schema);
