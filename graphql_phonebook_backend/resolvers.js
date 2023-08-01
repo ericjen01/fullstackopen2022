62eef03e7d46833aea9353e78088cf8f55fc0548
ericjen01 <ericjen01>
2023-08-01 09:42:05 -0700

    8-e3: refactoring the backend

---
+++ graphql_phonebook_backend/resolvers.js
@@ -0,0 +1,128 @@
+const Person = require("./models/person");
+const { GraphQLError } = require("graphql");
+const User = require("./models/user");
+const jwt = require("jsonwebtoken");
+
+const resolvers = {
+  Query: {
+    personCount: async () => Person.collection.countDocuments(),
+    allPersons: async (root, args) => {
+      if (!args.phone) {
+        return Person.find({});
+      }
+      return Person.find({ phone: { $exists: args.phone === "YES" } });
+    },
+    findPerson: async (root, args) => Person.findOne({ name: args.name }),
+    me: (root, args, context) => {
+      return context.currentUser;
+    },
+  },
+
+  Person: {
+    address: ({ street, city }) => {
+      return {
+        street,
+        city,
+      };
+    },
+  },
+
+  Mutation: {
+    addPerson: async (root, args, context) => {
+      const person = new Person({ ...args });
+      //console.log("**person, index.js: ", person);
+      const currentUser = context.currentUser;
+      //console.log("** currentUser, index.js: ", currentUser);
+      //console.log("** mutation-addPerson- context, index.js: ", context);
+      //console.log("** mutation-addPerson- context.currentUser: ",context.currentUser);
+      if (!currentUser) {
+        throw new GraphQLError("not authenticated", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+          },
+        });
+      }
+
+      try {
+        await person.save();
+
+        currentUser.friends = currentUser.friends.concat(person);
+        await currentUser.save();
+
+        //
+      } catch (error) {
+        throw new GraphQLError("Saving Person Failed: ", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.name,
+            error,
+          },
+        });
+      }
+      return person;
+    },
+
+    addAsFriend: async (root, args, { currentUser }) => {
+      const isFriend = (person) =>
+        currentUser.friends
+          .map((f) => f._id.toString())
+          .includes(person._id.toString());
+
+      if (!currentUser) {
+        throw new GraphQLError("wrong credentials", {
+          extensions: { code: "BAD_USER_INPUT" },
+        });
+      }
+
+      const person = await Person.findOne({ name: args.name });
+      if (!isFriend(person)) {
+        currentUser.friends = currentUser.friends.concat(person);
+      }
+    },
+
+    editNumber: async (root, args) => {
+      const person = await Person.findOne({ name: args.name });
+      person.phone = args.phone;
+      try {
+        await person.save();
+      } catch (error) {
+        throw new GraphQLError("editing number failed", {
+          extensions: {
+            code: "BAS_USER_INPUT",
+            invalidArgs: args.name,
+            error,
+          },
+        });
+      }
+      return person;
+    },
+
+    createUser: async (root, args) => {
+      const user = new User({ username: args.username });
+      return user.save().catch((error) => {
+        throw new GraphQLError("User Creation Failed", {
+          extensions: { code: "BAD_USER_INPUT", invalidArgs: args.name, error },
+        });
+      });
+    },
+
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username });
+
+      if (!user || args.password !== "secret") {
+        throw new GraphQLError("wrong credentials", {
+          extensions: { code: "BAD_USER_NAME" },
+        });
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      };
+
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) };
+    }, //login
+  }, //Mutation
+}; //resolvers
+
+module.exports = resolvers;
