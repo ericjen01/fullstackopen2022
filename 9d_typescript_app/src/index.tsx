6986737c201ad0e2768f96e0b05b2358b2ef65f3
ericjen01 <ericjen01>
2023-08-12 15:19:48 -0700

    9d-2: react components with typescript

---
+++ 9d_typescript_app/src/index.tsx
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom/client'
+import PropTypes from "prop-types";
+
+interface WelcomeProps{
+  name: string
+}
+
+// {name} as destructured prop from <Welcome name="Sarah"/>
+// {name:string} as prop from WelcomeProps
+// return type is JSX.Element
+const Welcome = ({ name }: { name: string }): JSX.Element => (
+  <h1>Hello, {name}</h1>
+);
+
+// type assertion "as HTMLElement". search 9d "type assertion" for explianation
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <Welcome name="Sarah" />
+)
