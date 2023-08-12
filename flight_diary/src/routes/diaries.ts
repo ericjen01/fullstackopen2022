a01af33c6acb686ad32e01aaad4f368a8ec2281b
ericjen01 <ericjen01>
2023-08-11 23:59:53 -0700

    9c-9: adding a new diary

--- flight_diary/src/routes/diaries.ts
+++ flight_diary/src/routes/diaries.ts
@@ -1,5 +1,6 @@
 import express  from "express";
 import diaryService from "../services/diaryService";
+import toNewDiaryEntry from "../utils"
 const router = express.Router()
 
 router.get("/",(_req,res)=>{
@@ -8,8 +9,20 @@ router.get("/",(_req,res)=>{
   //  console.log("* dirayService.getNonSensitiveEntries(): ", diaryService.getNonSensitiveEntries() )
 })
 
-router.post('/', (_req,res)=>{
-    res.send('Saving all Diaries')
+router.get("/:id",(req,res)=>{
+    const diary = diaryService.findById(Number(req.params.id))
+    if(diary){
+        res.send(diary)
+    }else{
+        res.sendStatus(404)
+    }
+})
+
+router.post('/', (req,res)=>{ //adding a new diary entry
+    const {date, weather, visibility, comment} = req.body
+    //destructuring diaryService.addDairy {date, weather..ect} allows us to extract multiple pieces of data from an array or object and assign them to their own variables.
+    const newDiaryEntry = diaryService.addDiary({date, weather, visibility, comment}) 
+    res.json(newDiaryEntry)
 })
 
 export default router
