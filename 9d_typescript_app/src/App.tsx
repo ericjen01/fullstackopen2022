eaab8de946f0b4f9d9895d701e2d2d8a2c9cdf37
ericjen01 <ericjen01>
2023-08-14 09:48:51 -0700

    9d-3: exercise 9.14

--- 9d_typescript_app/src/App.tsx
+++ 9d_typescript_app/src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { JsxElement } from "typescript";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
+
+interface propType{
+  courseName?: string, //"?" = optional
+  content?: string, 
+  count?:number,
+  total?: number,
 }
+
+  const Header =(props:propType )=>{
+   return <h2>{props.courseName}</h2>
+  }
+
+  const Content = (props: propType) =>{
+    return <p>{props.content}, score: {props.count}</p>
+  }
+
+ const Total = (props:propType)=>{
+  return <h4>Total Credit: {props.total}</h4> 
+ }
+
+  return (
+<div>
+  <Header courseName={courseName}/>
+  <Content content={courseParts[0].name} count={courseParts[0].exerciseCount}/> 
+  <Total total={courseParts[0].exerciseCount}/>
+</div>
+  
+    
+  );
+};
 
 export default App;
