7ab067a5678ad9b2cda508335d55d44fde1467f2
ericjen01 <ericjen01>
2023-08-16 16:30:35 -0700

    9d-5: more type narrowing

--- 9d_typescript_app/src/App.tsx
+++ 9d_typescript_app/src/App.tsx
@@ -37,39 +37,50 @@ interface propType{
   count?:number,
   total?: number,
 }
-
 interface CoursePartBase{
   name: string;
   exerciseCount: number;
 }
-
 interface CoursePartBasic extends CoursePartBase  {
   description: string;
   kind: "basic"
 }
-
 interface CoursePartGroup extends CoursePartBase{
-
   groupProjectCount: number;
   kind: "group"
 }
-
 interface CoursePartBackground extends CoursePartBase{
   description: string;
   backgroundMaterial: string;
   kind: "background"
 }
-
 type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
+
+
+//search "exhaust type checking" in 9-c5
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+
+courseParts.forEach(p=>{
+  switch(p.kind){
+    case "basic": console.log(p.name, p.description, p.exerciseCount); break;
+    case "group": console.log(p.name, p.exerciseCount, p.groupProjectCount); break;
+    case "background": console.log(p.description, p.backgroundMaterial ); break;
+    //default: break;
+    default: return assertNever(p); break;
+  }
+})
 
   const Header =(props:propType )=>{
    return <h2>{props.courseName}</h2>
   }
-
   const Content = (props: propType) =>{
     return <p>{props.content}, score: {props.count}</p>
   }
-
  const Total = (props:propType)=>{
   return <h4>Total Credit: {props.total}</h4> 
  }
