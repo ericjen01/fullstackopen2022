06148b6ca2728334b17434e568a9162b10fe037c
ericjen01 <ericjen01>
2023-10-25 02:59:49 -0700

    exercise_9.27_5: fixed select menu errors

--- patientor_frontend/src/services/diagnoses.ts
+++ patientor_frontend/src/services/diagnoses.ts
@@ -2,13 +2,18 @@ import axios from 'axios';
 import {Diagnosis} from "../types"
 import { apiBaseUrl } from '../constants'; //"3001/api"
 
-const allEntries = async()=>{
+const allEntries = async() => {
     const{data} = await axios.get<Diagnosis[]>(`${apiBaseUrl}/diagnoses/all`)
     return data
 }
 
+const diagnosisCodes = async() => {
+  const{data} = await axios.get<Diagnosis["code"][]>(`${apiBaseUrl}/diagnosisCodes`)
+  return data
+}
+
 const exportedObject = {
-  allEntries,
+  allEntries, diagnosisCodes
 };
 
 export default exportedObject;
